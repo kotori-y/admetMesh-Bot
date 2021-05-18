70dd89fbaebaff1bb3c117558b9932fd271bcc69
Zhijiang Yang <yzjkid9@gmail.com>
2021-05-18 20:13:54 +0800

    :tada: init

---
+++ README.md
@@ -0,0 +1,10 @@
+<!--
+ * @Description: 
+ * @Author: Kotori Y
+ * @Date: 2021-05-18 19:37:17
+ * @LastEditors: Kotori Y
+ * @LastEditTime: 2021-05-18 20:12:36
+ * @FilePath: \admetMesh-bot\readme.md
+ * @AuthorMail: kotori@cbdd.me
+-->
+# admetMesh-bot
---
+++ index.js
@@ -0,0 +1,79 @@
+/*
+ * @Description: post admetMesh (https://admetmesh.scbdd.com/)
+ * @Author: Kotori Y
+ * @Date: 2021-05-18 14:03:30
+ * @LastEditors: Kotori Y
+ * @LastEditTime: 2021-05-18 20:07:52
+ * @FilePath: \admetMesh-bot\index.js
+ * @AuthorMail: kotori@cbdd.me
+ */
+
+const fetch = require("node-fetch");
+const cheerio = require("cheerio");
+const FormData = require("form-data");
+
+async function getTokenCookie() {
+  const visitUrl = "https://admetmesh.scbdd.com/service/screening/index";
+
+  var resp = await fetch(visitUrl, {
+    method: "GET",
+    timeout: 180000,
+  });
+
+  var html = await resp.text();
+  var $ = cheerio.load(html);
+  const token = $("input[name='csrfmiddlewaretoken']").val();
+  const cookie = resp.headers.raw()["set-cookie"][0];
+  return [token, cookie];
+}
+
+async function predict(smiles) {
+  const [token, cookie] = await getTokenCookie();
+  const postUrl = "https://admetmesh.scbdd.com/service/screening/cal";
+
+  const headers = {
+    Origin: "https://admetmesh.scbdd.com",
+    Referer: "https://admetmesh.scbdd.com/service/screening/index",
+    Cookie: cookie,
+  };
+
+  const form = new FormData();
+  form.append("csrfmiddlewaretoken", token);
+  form.append("smiles-list", smiles);
+  form.append("method", "2");
+  form.append("X-CSRFTOKEN", token);
+
+  const resp = await fetch(postUrl, {
+    method: "POST",
+    timeout: 180000,
+    body: form,
+    headers: headers,
+  });
+
+  html = await resp.text();
+  $ = cheerio.load(html);
+  const finUrl = $("input[name='file_path']").val();
+  return finUrl;
+}
+
+async function getRes(smiles) {
+  let finUrl = await predict(smiles);
+  finUrl = `https://admetmesh.scbdd.com${finUrl}`;
+
+  const resp = await fetch(finUrl, {
+    method: "GET",
+    timeout: 180000,
+  });
+
+  const res = await resp.text();
+  return res;
+}
+
+(async () => {
+  try {
+    html = await getRes("c1ccccc1");
+    console.log(html);
+  } catch (e) {
+    console.log(e);
+  }
+})();
