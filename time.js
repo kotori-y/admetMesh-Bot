403797600853a6df2af387740a227cf3664d7f9e
Zhijiang Yang <yzjkid9@gmail.com>
2021-05-18 21:19:07 +0800

    adjusted layout

--- time.js
+++ time.js
@@ -3,14 +3,14 @@
  * @Author: Kotori Y
  * @Date: 2021-05-18 21:12:04
  * @LastEditors: Kotori Y
- * @LastEditTime: 2021-05-18 21:13:24
+ * @LastEditTime: 2021-05-18 21:18:28
  * @FilePath: \admetMesh-bot\time.js
  * @AuthorMail: kotori@cbdd.me
  */
 const readme = `\
 # admetMesh-Bot
 ---
-⏰ Updated on ${new Date().toUTCString()}
+⏰ Updated on ${new Date().toUTCString()}\
 [![admetMesh-Bot](https://github.com/kotori-y/admetMesh-bot/actions/workflows/main.yml/badge.svg)](https://github.com/kotori-y/admetMesh-bot/actions/workflows/main.yml)\
 `
 
